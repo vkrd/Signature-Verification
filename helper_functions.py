4e51a67f0f64b95c1b7a0005a44db523c57c3f26
Vikram <vduvvur2@illinois.edu>
2020-04-10 17:10:07 -0500

    Added helper function, optimized input shape, added comments, cleaned up code

--- helper_functions.py
+++ helper_functions.py
@@ -10,7 +10,7 @@ from keras.preprocessing import image
 from keras.layers import *
 from keras import Model, Sequential
 
-input_shape = (512, 128, 1)
+input_shape = (650, 275, 1)
 train_users = []
 test_users = []
 
@@ -68,13 +68,14 @@ def create_network():
     network.add(Dropout(0.25))
 
     network.add(Flatten())
-    #network.add(Dense(256, activation='relu'))
-    #network.add(Dropout(0.5))
+    network.add(Dense(256, activation='relu'))
+    network.add(Dropout(0.5))
     network.add(Dense(128, activation='softmax'))
 
-    # Normalization Layer
+    # Normalization Layer (need this for all models)
     network.add(Lambda(lambda x: keras.backend.l2_normalize(x, axis=-1)))
 
+    # Use this for non-sequential models
     # output = Lambda(lambda x: keras.backend.l2_normalize(x, axis=-1))(model.output)
 
     return network
@@ -82,6 +83,7 @@ def create_network():
 def load_data():
     print("Mapping data...")
     global train_users, test_users
+    # Locate all train and test images
     train_users = [f.path for f in os.scandir("./data/train") if (f.is_dir() and not f.path[-1] == "g")]
     test_users = [f.path for f in os.scandir("./data/test") if (f.is_dir() and not f.path[-1] == "g")]
     print("Mapped all data")
@@ -111,8 +113,6 @@ def get_batch_random(batch_size, dataset="train", agents=None, chunksize=1):
 
         triplet_labels.append([pos[0], pos[1], neg])
 
-        # print("Anchor = " + pos[0] + " Pos = " + pos[1] + " Neg = " + neg)
-
     with Pool(processes=agents) as pool:
         ret_triplet = pool.map(load_triplet, triplet_labels, chunksize=chunksize)
     return ret_triplet
@@ -125,17 +125,34 @@ def get_mixed_batch(sample_batch_size, hard_batch_size, normal_batch_size, model
     positive_embeddings = model.predict(batch[:,1,:,:], batch_size=sample_batch_size)
     negative_embeddings = model.predict(batch[:,2,:,:], batch_size=sample_batch_size)
 
-    # Calculate how loss for each triplet
+    # Calculate loss for each triplet
     sample_batch_losses = np.sum(np.square(anchor_embeddings - positive_embeddings), axis=1) - np.sum(np.square(anchor_embeddings - negative_embeddings), axis=1)
 
     sorted_batch = np.argsort(sample_batch_losses)[::-1]
 
+    # Pull hardest triplets
     hard_triplets = sorted_batch[:hard_batch_size]
+
+    # Pull random triplets
     random_triplets = random.choices(sorted_batch[hard_batch_size:], k=normal_batch_size)
 
     selection = np.append(hard_triplets, random_triplets)
 
-    # triplets = [batch[0][selection,:,:,:], batch[1][selection,:,:,:], batch[2][selection,:,:,:]]
     triplets = [batch[selection,0,:,:], batch[selection,1,:,:], batch[selection,2,:,:]]
 
     return triplets
+
+# Function to calculate ideal input shape
+def calculate_average_shape():
+    sumWidth = 0.0
+    sumHeight = 0.0
+    count = 0.0
+    for subdir, dirs, files in os.walk("./data"):
+        for file in files:
+            # print os.path.join(subdir, file)
+            filepath = subdir + os.sep + file
+            img = image.img_to_array(image.load_img(filepath, color_mode="grayscale"))
+            sumWidth += img.shape[1]
+            sumHeight += img.shape[0]
+            count += 1
+    return [sumWidth/count, sumHeight/count]
--- train.py
+++ train.py
@@ -8,20 +8,23 @@ network = create_network()
 model = create_model(network)
 model.compile(Adam())
 
-model.summary()
+# Uncomment to find out best input shape for model (default best is [275, 656])
+# print(calculate_average_shape())
 
 print("Training starting...\n")
 
-epochs = 50
+epochs = 10
 start = time.time()
+prev = start
 
-model_updates = 5
+model_updates = 1
 
 for i in range(epochs):
-    pass
     triplets = get_mixed_batch(64, 16, 16, network)
     loss = model.train_on_batch(triplets, None)
     if i % model_updates == 0:
-        print("Epoch: " + str(i+1) + "/" + str(epochs) + " Time Lapsed: " + str(time.time()-start) + " seconds Loss: " + str(loss))
+        print("Epoch: " + str(i+1) + "/" + str(epochs) + " | Time Lapsed: " + str(round(time.time()-prev, 1)) + " seconds | Loss: " + str(loss))
+    prev = time.time()
+
 
 print("Training complete")
